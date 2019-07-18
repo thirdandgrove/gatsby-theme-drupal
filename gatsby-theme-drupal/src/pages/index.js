4575c56408bca0cc4a89a09496ed8acc05551888
Grant Glidewell <ggcadc@gmail.com>
2019-07-18 15:27:07 -0700

    added list of entity types and routes on index page

--- gatsby-theme-drupal/src/pages/index.js
+++ gatsby-theme-drupal/src/pages/index.js
@@ -1,16 +1,55 @@
 import React from 'react';
+import { graphql, Link } from 'gatsby';
 
 import Layout from '../components/Layout';
 
-export default () => (
-  <Layout>
-    <ul>
-      <li>make a query</li>
-      <li>describe component shadowing</li>
-      <li>list entity types</li>
-      <li>show how to route</li>
-      <li>show how to query</li>
-      <li>show how to template and generate dynamic pages</li>
-    </ul>
-  </Layout>
-);
+export default ({ data }) => {
+  const entityTypes = data.allSitePage.nodes.reduce((acc, node) => {
+    const entityType = node.path.split('/')[1];
+    if (
+      entityType &&
+      !acc.includes(entityType) &&
+      entityType !== 'dev-404-page'
+    ) {
+      acc.push(entityType);
+    }
+    return acc;
+  }, []);
+
+  return (
+    <Layout>
+      <h1>Entity Types</h1>
+      <ul>
+        {entityTypes.map(entity => (
+          <li>{entity}</li>
+        ))}
+      </ul>
+      <h1>Routes</h1>
+      <ul>
+        {data.allSitePage.nodes.map(node => (
+          <li>
+            <Link to={node.path}>{node.path}</Link>
+          </li>
+        ))}
+      </ul>
+      <ul>
+        <li>make a query</li>
+        <li>describe component shadowing</li>
+        <li>list entity types</li>
+        <li>show how to route</li>
+        <li>show how to query</li>
+        <li>show how to template and generate dynamic pages</li>
+      </ul>
+    </Layout>
+  );
+};
+
+export const query = graphql`
+  {
+    allSitePage {
+      nodes {
+        path
+      }
+    }
+  }
+`;
