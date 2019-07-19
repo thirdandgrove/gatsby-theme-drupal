7e59e3cdfb95c2dce05a2a2829bb097b6efad9e5
Grant Glidewell <ggcadc@gmail.com>
2019-07-18 21:03:12 -0700

    actually showing entity types, not mapping routes

--- gatsby-theme-drupal/src/pages/index.js
+++ gatsby-theme-drupal/src/pages/index.js
@@ -3,44 +3,30 @@ import { graphql, Link } from 'gatsby';
 
 import Layout from '../components/Layout';
 
-export default ({ data }) => {
-  const entityTypes = data.allSitePage.nodes.reduce((acc, node) => {
-    const entityType = node.path.split('/')[1];
-    if (
-      entityType &&
-      !acc.includes(entityType) &&
-      entityType !== 'dev-404-page'
-    ) {
-      acc.push(entityType);
-    }
-    return acc;
-  }, []);
-
-  return (
-    <Layout>
-      <h1>Entity Types</h1>
-      <ul>
-        {entityTypes.map(entity => (
-          <li key={entity}>{entity}</li>
-        ))}
-      </ul>
-      <h1>Routes</h1>
-      <ul>
-        {data.allSitePage.nodes.map(node => (
-          <li key={node.path}>
-            <Link to={node.path}>{node.path}</Link>
-          </li>
-        ))}
-      </ul>
-      <ul>
-        <li>describe component shadowing</li>
-        <li>show how to route</li>
-        <li>show how to query</li>
-        <li>show how to template and generate dynamic pages</li>
-      </ul>
-    </Layout>
-  );
-};
+export default ({ data }) => (
+  <Layout>
+    <h1>Entity Types</h1>
+    <ul>
+      {data.allNodeTypeNodeType.nodes.map(entity => (
+        <li key={entity.drupal_internal__type}>{entity.name}</li>
+      ))}
+    </ul>
+    <h1>Routes</h1>
+    <ul>
+      {data.allSitePage.nodes.map(node => (
+        <li key={node.path}>
+          <Link to={node.path}>{node.path}</Link>
+        </li>
+      ))}
+    </ul>
+    <ul>
+      <li>describe component shadowing</li>
+      <li>show how to route</li>
+      <li>show how to query</li>
+      <li>show how to template and generate dynamic pages</li>
+    </ul>
+  </Layout>
+);
 
 export const query = graphql`
   {
@@ -48,6 +34,13 @@ export const query = graphql`
       nodes {
         path
       }
+    }
+    allNodeTypeNodeType {
+      nodes {
+        name
+        drupal_internal__type
+      }
+      totalCount
     }
   }
 `;
